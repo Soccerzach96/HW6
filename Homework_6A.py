4efe2bd8779e2cc04d8f49d69fee478e76acb725
Zach <strongz@umich.edu>
2017-10-14 15:25:47 -0400

    Completed Part A

--- Homework_6A.py
+++ Homework_6A.py
@@ -9,6 +9,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
+import re
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
@@ -17,17 +18,33 @@ ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
 html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+sum = 0
+
+tags = soup('span')
+for tag in tags:
+	y = str(tag)
+	x = re.findall('[0-9]+',y)
+	for i in x:
+		i = int(i)
+		sum += i
+print(sum)
 
 # html.parser is the HTML parser included in the standard Python 3 library.
 # information on other HTML parsers is here:
 # http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
-soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    # Look at the parts of a tag
-    print('TAG:', tag)
-    print('URL:', tag.get('href', None))
-    print('Contents:', tag.contents[0])
-    print('Attrs:', tag.attrs)
+
+# count = soup.findAll('span', {'class':'comments'})
+# textSum = [number.text for number in count]
+
+
+# tags = soup('a')
+# for tag in tags:
+#     # Look at the parts of a tag
+#     print('TAG:', tag)
+#     print('URL:', tag.get('href', None))
+#     print('Contents:', tag.contents[0])
+#     print('Attrs:', tag.attrs)
--- Homework_6B.py
+++ Homework_6B.py
@@ -18,7 +18,46 @@ url = input('Enter - ')
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
+count = int(input("Enter count: "))
+position = int(input("Enter position: "))
+
+names = []
+
+while count > 0:
+	soup = BeautifulSoup(html, 'html.parser')
+	tags = soup('a')
+	name = tags[position-1]
+	names.append(name)
+	html = tags[position-1]['href']
+	count -= 1
+
+print(names[-1])
+
+
+# for x in range(count):
+# 	soup = BeautifulSoup(html, 'html.parser')
+# 	tags = soup('a')
+	# for tag in tags:
+	# 	name = tag[position]
+	# 	names.append(name)
+	# 	names.append(tag.get('href', None))
+
+
 # Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+# tags = soup('a')
+# for tag in tags:
+#     print(tag.get('href', None))
+
+# count = int(input("Enter count: "))
+# position = int(input("Enter position: "))
+
+# names = []
+# url = urllib.request.urlopen(names[position - 1], context = ctx).read()
+
+# for x in range(count):
+# 	names = []
+# 	html = urlib.request.urlopen(url, context=ctx).read()
+# 	soup = BeautifulSoup(html, 'html.parser')
+# 	tags = soup('a')
+# 	for tag in tags:
+# 		names.append(tag.get('href', None))
